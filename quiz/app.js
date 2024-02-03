844fe31c581c913eb47dfff9c68e2bbb1a27fd37
RiverCarley <river.carley@ocadu.ca>
2024-02-03 13:04:23 -0500

    added 27 cases and images

--- quiz/app.js
+++ quiz/app.js
@@ -65,23 +65,195 @@ const answers = [
     {
         combination: ["Sparkling Water", "Sea Monkeys", "1965 Shelby Cobra"],
         text: "1",
-
+        textb: "1",
+     
     },
     {
         combination: ["Sparkling Water", "Sea Monkeys", "2024 Volvo S60"],
         text: "2",
+        textb: "2",
+
 
     },
     {  
         combination: ["Sparkling Water", "Sea Monkeys", "Tesla Model 3"],
         text: "3",
+        textb: "3",
+    
    
     },
     {
         combination: ["Sparkling Water", "Rabbit", "1965 Shelby Cobra"],
-        text: "4",
+    
+       
+        text: "The Road by Cormac McCarthy",
+        textb: "4",
   
-    }
+    },
+    {
+        combination: ["Sparkling Water", "Rabbit", "2024 Volvo S60"],
+        text: "5",
+        textb: "5",
+
+
+    },
+    {  
+        combination: ["Sparkling Water", "Rabbit", "Tesla Model 3"],
+        text: "6",
+        textb: "6",
+    
+   
+    },
+    {
+        combination: ["Sparkling Water", "Dog", "1965 Shelby Cobra"],
+    
+       
+        text: "7",
+        textb: "7",
+  
+    },
+    {
+        combination: ["Sparkling Water", "Dog", "2024 Volvo S60"],
+        text: "8",
+        textb: "8",
+
+
+    },
+    {  
+        combination: ["Sparkling Water", "Dog", "Tesla Model 3"],
+        text: "9",
+        textb: "9",
+    
+   
+    },
+     {
+        combination: ["Psilocybin Tea", "Sea Monkeys", "1965 Shelby Cobra"],
+        text: "10",
+        textb: "10",
+     
+    },
+    {
+        combination: ["Psilocybin Tea", "Sea Monkeys", "2024 Volvo S60"],
+        text: "11",
+        textb: "11",
+
+
+    },
+    {  
+        combination: ["Psilocybin Tea", "Sea Monkeys", "Tesla Model 3"],
+        text: "12",
+        textb: "12",
+    
+   
+    },
+    {
+        combination: ["Psilocybin Tea", "Rabbit", "1965 Shelby Cobra"],
+    
+       
+        text: "13",
+        textb: "13",
+  
+    },
+    {
+        combination: ["Psilocybin Tea", "Rabbit", "2024 Volvo S60"],
+        text: "14",
+        textb: "14",
+
+
+    },
+    {  
+        combination: ["Psilocybin Tea", "Rabbit", "Tesla Model 3"],
+        text: "15",
+        textb: "15",
+    
+   
+    },
+    {
+        combination: ["Psilocybin Tea", "Dog", "1965 Shelby Cobra"],
+    
+       
+        text: "16",
+        textb: "16",
+  
+    },
+    {
+        combination: ["Psilocybin Tea", "Dog", "2024 Volvo S60"],
+        text: "17",
+        textb: "17",
+
+
+    },
+    {  
+        combination: ["Psilocybin Tea", "RaDogbbit", "Tesla Model 3"],
+        text: "18",
+        textb: "18",
+    
+   
+    },
+    {
+        combination: ["Cream Soda", "Sea Monkeys", "1965 Shelby Cobra"],
+        text: "19",
+        textb: "19",
+     
+    },
+    {
+        combination: ["Cream Soda", "Sea Monkeys", "2024 Volvo S60"],
+        text: "20",
+        textb: "20",
+
+
+    },
+    {  
+        combination: ["Cream Soda", "Sea Monkeys", "Tesla Model 3"],
+        text: "21",
+        textb: "21",
+    
+   
+    },
+    {
+        combination: ["Cream Soda", "Rabbit", "1965 Shelby Cobra"],
+    
+       
+        text: "22",
+        textb: "22",
+  
+    },
+    {
+        combination: ["Cream Soda", "Rabbit", "2024 Volvo S60"],
+        text: "23",
+        textb: "23",
+
+
+    },
+    {  
+        combination: ["Cream Soda", "Rabbit", "Tesla Model 3"],
+        text: "24",
+        textb: "24",
+    
+   
+    },
+    {
+        combination: ["Cream Soda", "Dog", "1965 Shelby Cobra"],
+    
+       
+        text: "25",
+        textb: "25",
+  
+    },
+    {
+        combination: ["Cream Soda", "Dog", "2024 Volvo S60"],
+        text: "26",
+        textb: "26",
+
+
+    },
+    {  
+        combination: ["Cream Soda", "Dog", "Tesla Model 3"],
+        text: "27",
+        textb: "27",
+    
+   
+    },
 ]
 // need to have a default answer to compensate for lack of combination data
 
@@ -148,29 +320,58 @@ const showAnswer = () => {
     let result
     answers.forEach(answer => {
         if (
-            chosenAnswers.includes(answer.combination[0]) +
-            chosenAnswers.includes(answer.combination[1]) 
+            chosenAnswers.includes(answer.combination[0])+
+            chosenAnswers.includes(answer.combination[1])+
+            chosenAnswers.includes(answer.combination[2])+
+            chosenAnswers.includes(answer.combination[3])+
+            chosenAnswers.includes(answer.combination[4])+
+            chosenAnswers.includes(answer.combination[5])+
+            chosenAnswers.includes(answer.combination[6])+
+            chosenAnswers.includes(answer.combination[7])+
+            chosenAnswers.includes(answer.combination[8])+
+            chosenAnswers.includes(answer.combination[9])+
+            chosenAnswers.includes(answer.combination[10])+
+            chosenAnswers.includes(answer.combination[11])+
+            chosenAnswers.includes(answer.combination[12])+
+            chosenAnswers.includes(answer.combination[13])+
+            chosenAnswers.includes(answer.combination[14])+
+            chosenAnswers.includes(answer.combination[15])+
+            chosenAnswers.includes(answer.combination[16])+
+            chosenAnswers.includes(answer.combination[17])+
+            chosenAnswers.includes(answer.combination[18])+
+            chosenAnswers.includes(answer.combination[19])+
+            chosenAnswers.includes(answer.combination[20])+
+            chosenAnswers.includes(answer.combination[21])+
+            chosenAnswers.includes(answer.combination[22])+
+            chosenAnswers.includes(answer.combination[23])+
+            chosenAnswers.includes(answer.combination[24])+
+            chosenAnswers.includes(answer.combination[25])+
+            chosenAnswers.includes(answer.combination[26])
         ) {
             result = answer
             return
-        } else if (!result) {
+        } //else if (!result) {
             //first answer object is default
-            result = answers[0]
-        }
+         //   result = answers[0]
+       // }
     })
 
     const answerBlock = document.createElement('div')
     answerBlock.classList.add('result-block')
     const answerTitle = document.createElement('h3')
     answerTitle.textContent = result.text
+    function ReplaceImage(y){
+        document.getElementById("x").src=y
+    }
     
 
     answerBlock.append(answerTitle)
 
     answerDisplay.append(answerBlock)
-
+    ReplaceImage("./images/"+result.textb+".jpg");
     const allAnswerBlocks = document.querySelectorAll('.answer-block')
     Array.from(allAnswerBlocks).forEach(answerBlock => answerBlock.replaceWith(answerBlock.cloneNode(true)))
+  
 
 }
 
